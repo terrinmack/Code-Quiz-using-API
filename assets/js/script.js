59fdb808dafd1302fbb5b01896161e0bce722496
Terri Mack <terrimack15@gmail.com>
2023-01-21 22:36:14 -0800

    edited html and css files

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        title: "Commonly used data types do not include:",
+        choices: ["strings", "booleans", "alerts", "numbers",],
+        correctAnswer: "alerts",
+    },
+    {
+        title: "The condition in an if / else statement is enclosed with ____ ?",
+        choices: ["quotes", "curly brackets", "parenthesis", "square brackets",],
+        correctAnswer: "parenthesis",
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ____ ?",
+        choices: ["Numbers & Strings", "Other arrays", "Booleans", "All of the above",],
+        correctAnswer: "All of the above",
+    },
+    {
+        title: "String Values must be enclosed within ____ when being assigned to variables.",
+        choices: ["Commas", "Curly brackets", "Quotes", "Parenthesis",],
+        correctAnswer: "Quotes",
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "Terminal/Bash", "for loops", "console.log",],
+        correctAnswer: "console.log",
+    },
+];
+
