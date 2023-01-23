b70e92b1c4825127e548272fc40f0244dab97cf6
Terri Mack <terrimack15@gmail.com>
2023-01-23 15:23:55 -0800

    added question array in script

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// question aray
+var questions = [
+    { q: 'Commonly used data types do not include:', 
+          a: '3. alerts', 
+          choices: [{choice: '1. string'}, {choice: '2. booleans'}, {choice: '3. alerts'}, {choice: '4. numbers'}]
+    },
+    { q: '"The condition in an if / else statement is enclosed with ____ ?', 
+          a: '3. paranthesis', 
+          choices: [{choice: '1. quotes'}, {choice: '2. curly brackets'}, {choice: '3. paranthesis'}, {choice: '4. square brackets'}]
+    },
+    { q: 'Arrays in Javascript can be used to store ____?', 
+          a: '4. all of the above', 
+          choices: [{choice: '1. numbers & strings'}, {choice: '2. other arrays'}, {choice: '3. booleans'}, {choice: '4. all of the above'}]
+    },
+    { q: 'String Values must be enclosed within ____ when being assigned to variables.', 
+          a: '3. quotes', 
+          choices: [{choice: '1. commas'}, {choice: '2. curly brackets'}, {choice: '3. quotes'}, {choice: '4. paranthesis'}]
+    },
+    { q: '"A very useful tool used during development and debugging for printing content to the debugger is:', 
+          a: '4. console.log', 
+          choices: [{choice: '1. javascript'}, {choice: '2.  terminal/bash'}, {choice: '3. for loops'}, {choice: '4. console.log'}]
+    },
+]
+// set variables by element
+
