ffee103b28ccf003f102883c3d3298f3dc0b2616
Terri Mack <terrimack15@gmail.com>
2023-01-25 19:10:50 -0800

    created a functioning timer and ability to show questions in java

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,58 +2,100 @@ var questions = [
     {
         // question 0
         title: "Commonly used data types do NOT include:",
-        choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-        correctAnswer: "3"
+        a: "Strings", 
+        b: "Booleans", 
+        c: "Alerts", 
+        d: "Numbers",
+        correctAnswer: "c"
     },
     {
         // question 1
         title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
-        correctAnswer: "3"
+        a: "Quotes", 
+        b: "Curly brackets", 
+        c: "Parentheses", 
+        d: "Square brackets",
+        correctAnswer: "c"
     },
     {
         // question 2
         title: "Arrays in Javascript can be used to store ____.",
-        choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
-        correctAnswer: "4"
+        a: "Numbers and strings", 
+        b: "Other arrays", 
+        c: "Booleans", 
+        d: "All of the above",
+        correctAnswer: "d"
     },
     {
         // question 3
         title: "String values must be enclosed within ____ when being assigned to variables.",
-        choices: ["1. commmas", "2. curly brackets", "3. quotes", "4. parentheses"],
-        correctAnswer: "3"
+        a: "Commmas", 
+        b: "Curly brackets", 
+        c: "Quotes", 
+        d: "Parentheses",
+        correctAnswer: "c"
     },
     {
         // question 4
-        questititleon: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choices: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
-        correctAnswer: "4"
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        a: "Javascript", 
+        b: "2. Terminal/bash", 
+        c: "3. For loops", 
+        d: "Console.log",
+        correctAnswer: "d"
     }
 ];
+
+// variables
+var timer;
+var timerCount = 75;
+var timerEl = document.querySelector('.time-left')
+var startContainerEl = document.querySelector('.start-container');
+var questionContainerEl = document.querySelector('.quiz-container');
+var viewHighscoreNav = document.querySelector('.view-highscores');
+var answersEls = document.querySelector('.answer');
+var questionEl = document.querySelector('.question');
+var a_text = document.querySelector('#a');
+var b_text = document.querySelector('#b');
+var c_text = document.querySelector('#c');
+var d_text = document.querySelector('#d');
+var startBtn = document.querySelector('.start-btn')
+var submitBtn = document.querySelector('.submit-btn')
+
+let currentQuestion = 0;
+let score = 0;
 
 // start quiz
 function startQuiz() {
     // hide start screen
-    startContainerEl.setAttribute('class', 'hide');
+    startContainerEl.setAttribute('id', 'hide');
     // show question screen
-    questionContainerEl.setAttribute('class', 'show');
+    questionContainerEl.setAttribute('id', 'show');
+    // hide viewhighscore button
+    var currentQuestionData = questions[currentQuestion];
+    questionEl.innerText = currentQuestionData.title;
+    a_text.innerText = currentQuestionData.a;
+    b_text.innerText = currentQuestionData.b;
+    c_text.innerText = currentQuestionData.c;
+    d_text.innerText = currentQuestionData.d;
     // start timer
     startTimer();
-    startQuiz();
+    console.log(questions)
 }
 
 // start timer
 function startTimer() {
-
     timer = setInterval(function() {
-        console.log(timer);
+
         timerCount--;
-        timerEl.textContent = timerCount
+        timerEl.innerHTML = timerCount;
 
          if (timerCount === 0) {
             clearInterval(timer);
             endQuiz();
         }
     }, 1000)
+    console.log(timer)
 }
 
+startBtn.addEventListener('click', startQuiz)
